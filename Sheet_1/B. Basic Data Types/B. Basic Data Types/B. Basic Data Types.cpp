2e0585be3a2c91c6b6146f292ce3a57b8ed2a652
Ahmed.net <Ahmed.net@gmail.com>
2023-06-03 00:25:09 +0300

    Problem solving projects

---
+++ Sheet_1/B. Basic Data Types/B. Basic Data Types/B. Basic Data Types.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int intnumber;
+	long long longnumber;
+	char charvar;
+	float floatval;
+	double doubleval;
+	cin >> intnumber >> longnumber >> charvar >> floatval >> doubleval;
+	cout << intnumber << endl << longnumber << endl << charvar << endl << floatval << endl << doubleval;
+	return 0;
+}
---
+++ Sheet_1/C. Simple Calculator/C. Simple Calculator/C. Simple Calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long Number1, Number2;
+    cin >> Number1 >> Number2;
+    cout << Number1 << " + " << Number2 << " = " << Number1 + Number2 << endl;
+    cout << Number1 << " * " << Number2 << " = " << Number1 * Number2 << endl;
+    cout << Number1 << " - " << Number2 << " = " << Number1 - Number2 << endl;
+
+    return 0;
+}
