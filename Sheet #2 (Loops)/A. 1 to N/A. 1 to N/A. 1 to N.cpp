2d908f884b5313a0e4a3b91f7705c7cc73cbef69
Ahmed.net <Ahmed.net@gmail.com>
2023-06-12 00:47:12 +0300

    Adding problem solving project

---
+++ Sheet #2 (Loops)/A. 1 to N/A. 1 to N/A. 1 to N.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << endl;
+	}
+    return 0;
+}
+
