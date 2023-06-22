b6cb47d91288a8783ea298d32cdea04dc0b62c21
Ahmed.net <Ahmed.net@gmail.com>
2023-06-22 13:17:51 +0300

    Add project solution

---
+++ Sheet_1/I. Welcome for you with Conditions/I. Welcome for you with Conditions/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void Speeding() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+int main()
+{
+    Speeding();
+    int a, b; cin >> a >> b;
+    cout << (a>=b) ? "Yes" : "No";
+    return 0;
+}
+
